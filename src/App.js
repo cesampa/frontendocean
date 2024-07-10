f506b66e375a4c805d08afc65503ab631a060bef
cesampa <cesampa@hotmail.com>
2024-07-10 00:12:59 -0300

    Implementados desafios de acréscimo e decréscimo de itens nocarrinho, proposto na aula

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 
 function MyButton() {
@@ -8,19 +9,37 @@ function MyButton() {
     );
 }
 
+function ItemDoCarrinho({ nomeDoItem, descricaoDoItem }) {
+
+    const [quantidade, setQuantidade] = useState(1);    
+
+    return (
+        <>
+            <h1>Item: {nomeDoItem}</h1>
+            <h4>Descrição: {descricaoDoItem}</h4>
+            <p>Quantidade: {quantidade}</p>
+            <button onClick={() => setQuantidade(quantidade + 1)}>Adicionar +1</button>
+            <button onClick={() => setQuantidade(quantidade - 1)}>Remover -1</button>
+        </>
+    )
+}
+
 function Saudacao({ nome }) {
     return (
         <h1>Olá, {nome}</h1>
     )
 }
-let nomeCompleto = "Carlos Sampaio"
+let item1 = "Panela";
+let item2 = "Caneca";
 export default function MyApp() {
     return (
         <div className='App'>
             <header className="App-header">
                 <h1>Bem-vindo ao meu aplicativo</h1>
-                <Saudacao nome={nomeCompleto} />
+                <ItemDoCarrinho nomeDoItem={item1} />
+                <ItemDoCarrinho nomeDoItem={item2} />
             </header>
         </div>
     );
 }
+
